7b3ce8f7178509f7cef4066de69d05e42d825f25
Cortella <bruno.machado@dreamlabs.com.br>
2023-01-07 06:37:08 -0300

    add likes funcionaty in social-network-template

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { Avatar } from './Avatar';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/cortella.png" />
@@ -16,7 +30,7 @@ export function Comment({ content }) {
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
@@ -25,9 +39,9 @@ export function Comment({ content }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
