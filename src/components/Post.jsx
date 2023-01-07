f09b69c496c22286cab998cffc6a7bd02ee6a6f5
Cortella <bruno.machado@dreamlabs.com.br>
2023-01-07 05:32:30 -0300

    create Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,6 @@
+import { Comment } from './Comment';
+import { Avatar } from './Avatar';
+
 import styles from './Post.module.css';
 
 export function Post() {
@@ -5,7 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/cortella.png" />
+        <Avatar src="https://github.com/cortella.png" />
           <div className={styles.authorInfo}>
             <strong>Nicolau Cortella</strong>
             <span>Web Developer</span>
@@ -24,6 +27,23 @@ export function Post() {
           <a href="">#nlw</a>{' '}
           <a href="">#rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,7 +11,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/cortella.png"/>
+      <Avatar src="https://github.com/maykbrito.png" />
         <strong> Nicolau Cortella</strong>
         <span>Web Developer</span>
       </div>
