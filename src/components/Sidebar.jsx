b612cfbff3f0ccff1de023010a673f37be1f6c2c
Cortella <bruno.machado@dreamlabs.com.br>
2023-01-07 05:18:15 -0300

    create post and sidebar components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return(
+    <aside  className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/cortella.png"/>
+        <strong> Nicolau Cortella</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar Perfil
+        </a>
+      </footer>
+    </aside>
+  )
+};
